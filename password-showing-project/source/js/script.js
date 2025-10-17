889ef7748503d5568abdf1120fe932fb58b33fd2
fateme shafiee <fatmshafiee@gmail.com>
2025-10-17 11:38:01 +0330

    password js

---
+++ password-showing-project/source/js/script.js
@@ -0,0 +1,14 @@
+﻿let passwordField=document.querySelector("#password-field");
+let iconEye=document.querySelector("#iconEye");
+
+
+iconEye.addEventListener('click',()=>{
+  if(passwordField.type=='password'){
+      passwordField.setAttribute('type','text')
+      iconEye.className="fa fa-eye-slash"
+  }else{
+     passwordField.setAttribute('type','password')
+      iconEye.className="fa fa-eye"
+  }
+
+})
